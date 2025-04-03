698c920faa122be8eb325ff04175effffdea9d7c
nviscio-polarier <nviscio.polarier@outlook.com>
2025-04-03 11:43:43 +0200

    hecho funcional

--- src/pages/components/GraficoAnual.js
+++ src/pages/components/GraficoAnual.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { BarChart, Grid, XAxis, YAxis } from "react-native-svg-charts";
 import { Line } from "react-native-svg";
@@ -9,19 +9,16 @@ import { colors } from "../../../styles/base";
 import BarraExpandible from "./BarraExpandible";
 import DetalleRendimiento from "./DetalleRendimiento";
 
-export class GraficoAnual extends React.PureComponent {
-  state = {
-    data: [],
-    labels: [],
-    añoActual: moment().year(),
-  };
+const GraficoAnual = () => {
+  const [data, setData] = useState([]);
+  const [labels, setLabels] = useState([]);
+  const [añoActual, setAñoActual] = useState(moment().year());
 
-  componentDidMount() {
-    this.cargarDatos();
-  }
+  useEffect(() => {
+    cargarDatos();
+  }, [añoActual]);
 
-  cargarDatos = () => {
-    const { añoActual } = this.state;
+  const cargarDatos = () => {
     const idPersona = 1526;
     const fechaIni = `${añoActual}-01-01`;
     const fechaFin = `${añoActual}-12-31`;
@@ -30,69 +27,20 @@ export class GraficoAnual extends React.PureComponent {
       .getRendimientoPersonaMaquina(idPersona, fechaIni, fechaFin)
       .then((response) => {
         console.log(`Datos recibidos para ${añoActual}:`, response);
-
-        const datosProcesados = this.datosProcesados(response);
-        this.setState({
-          data: datosProcesados.values,
-          labels: datosProcesados.labels,
-        });
+        const datosProcesados = procesarDatos(response);
+        setData(datosProcesados.values);
+        setLabels(datosProcesados.labels);
       })
       .catch((error) => {
         console.error("Error obteniendo datos:", error);
       });
   };
 
-  cambiarAño = (incremento) => {
-    this.setState(
-      (prevState) => ({ añoActual: prevState.añoActual + incremento }),
-      this.cargarDatos
-    );
+  const cambiarAño = (incremento) => {
+    setAñoActual((prevAño) => prevAño + incremento);
   };
 
-  // Aqui se actualiza de forma dinamica en funcion de si tiene o no registros
-
-  // datosProcesados(data) {
-  //   const informacionMensual = {};
-  //   const meses = [
-  //     "Ene",
-  //     "Feb",
-  //     "Mar",
-  //     "Abr",
-  //     "May",
-  //     "Jun",
-  //     "Jul",
-  //     "Ago",
-  //     "Sep",
-  //     "Oct",
-  //     "Nov",
-  //     "Dic",
-  //   ];
-
-  //   data.forEach((item) => {
-  //     const indiceMes = moment(item.fechaIni).month();
-  //     if (!informacionMensual[indiceMes]) {
-  //       informacionMensual[indiceMes] = { total: 0, count: 0 };
-  //     }
-  //     informacionMensual[indiceMes].total += item.RendimientoGlobal;
-  //     informacionMensual[indiceMes].count += 1;
-  //   });
-
-  //   const labels = Object.keys(informacionMensual).map(
-  //     (indiceMes) => meses[parseInt(indiceMes, 10)]
-  //   );
-
-  //   const values = Object.keys(informacionMensual).map(
-  //     (indiceMes) =>
-  //       (informacionMensual[indiceMes].total /
-  //         informacionMensual[indiceMes].count * 100)
-  //   );
-
-  //   return { labels, values };
-  // }
-
-  // Aqui renderiza los 12 meses y sus registros
-
-  datosProcesados(data) {
+  const procesarDatos = (data) => {
     const informacionMensual = {};
     const meses = [
       "Ene",
@@ -118,7 +66,6 @@ export class GraficoAnual extends React.PureComponent {
       informacionMensual[indiceMes].count += 1;
     });
 
-    const labels = meses;
     const values = Array(12)
       .fill(0)
       .map((_, i) => {
@@ -130,89 +77,76 @@ export class GraficoAnual extends React.PureComponent {
         return 0;
       });
 
-    return { labels, values };
-  }
+    return { labels: meses, values };
+  };
 
-  render() {
-    const { data, labels, añoActual } = this.state;
-
-    return (
-      <View style={styles.contenedor}>
-        {/* Cabecera */}
-        <View style={[styles.header, { backgroundColor: colors.primary }]}>
-          <Text style={styles.headerTexto}>Rendimiento anual</Text>
-        </View>
-
-        {/* Selector de año ( numero del año + flechas ) */}
-        <View style={styles.selectorAño}>
-          <TouchableOpacity onPress={() => this.cambiarAño(-1)}>
-            <MaterialCommunityIcons
-              style={styles.flechasSelectorAño}
-              name="chevron-left"
-              size={30}
-            />
-          </TouchableOpacity>
-          <Text style={styles.año}>{añoActual}</Text>
-          <TouchableOpacity onPress={() => this.cambiarAño(1)}>
-            <MaterialCommunityIcons
-              style={styles.flechasSelectorAño}
-              name="chevron-right"
-              size={30}
-            />
-          </TouchableOpacity>
-        </View>
-
-        {/* Tabla + Info % + Texto meses */}
-        <View style={{ flexDirection: "row", height: 220, padding: 20 }}>
-          {/* Numeros porcentaje lateral ( el % varia en funcion del rendimiento mas alto) */}
-          <YAxis
-            data={data}
-            contentInset={{ top: 10, bottom: 10 }}
-            svg={{ fontSize: 10, fill: "black" }}
-            formatLabel={(value) => `${value.toFixed(1)}%`}
-          />
-
-          {/* Tabala de contenidos */}
-          <View style={{ flex: 1, marginLeft: 10 }}>
-            <BarChart
-              style={styles.tabla}
-              data={data}
-              gridMin={0}
-              contentInset={{ top: 10, bottom: 10 }}
-              svg={{ fill: "rgb(237, 182, 55)" }}
-            >
-              <Grid />
-
-              {/* Linea gris de puntos para delimitar el porcentaje medio */}
-              <Line
-                x1="2%"
-                x2="98%"
-                y1={`${50}%`}
-                y2={`${50}%`}
-                stroke="grey"
-                strokeDasharray={[4, 7]}
-                strokeWidth={2}
-              />
-            </BarChart>
-
-            {/* Texto de los meses */}
-            <XAxis
-              data={labels}
-              formatLabel={(index) => labels[index]}
-              contentInset={{ left: 10, right: 10 }}
-              svg={{ fontSize: 10, fill: "black" }}
-            />
-          </View>
-        </View>
-
-        {/* Barra expandible ( sin funcioanar ni se le pasan datos) */}
-        <BarraExpandible>
-          <DetalleRendimiento />
-        </BarraExpandible>
+  return (
+    <View style={styles.contenedor}>
+      <View style={[styles.header, { backgroundColor: colors.primary }]}>
+        <Text style={styles.headerTexto}>Rendimiento anual</Text>
       </View>
-    );
-  }
-}
+
+      <View style={styles.selectorAño}>
+        <TouchableOpacity onPress={() => cambiarAño(-1)}>
+          <MaterialCommunityIcons
+            style={styles.flechasSelectorAño}
+            name="chevron-left"
+            size={30}
+          />
+        </TouchableOpacity>
+        <Text style={styles.año}>{añoActual}</Text>
+        <TouchableOpacity onPress={() => cambiarAño(1)}>
+          <MaterialCommunityIcons
+            style={styles.flechasSelectorAño}
+            name="chevron-right"
+            size={30}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={{ flexDirection: "row", height: 220, padding: 20 }}>
+        <YAxis
+          data={data}
+          contentInset={{ top: 10, bottom: 10 }}
+          svg={{ fontSize: 10, fill: "black" }}
+          formatLabel={(value) => `${value.toFixed(1)}%`}
+        />
+
+        <View style={{ flex: 1, marginLeft: 10 }}>
+          <BarChart
+            style={styles.tabla}
+            data={data}
+            gridMin={0}
+            contentInset={{ top: 10, bottom: 10 }}
+            svg={{ fill: "rgb(237, 182, 55)" }}
+          >
+            <Grid />
+            <Line
+              x1="2%"
+              x2="98%"
+              y1={`${50}%`}
+              y2={`${50}%`}
+              stroke="grey"
+              strokeDasharray={[4, 7]}
+              strokeWidth={2}
+            />
+          </BarChart>
+
+          <XAxis
+            data={labels}
+            formatLabel={(index) => labels[index]}
+            contentInset={{ left: 10, right: 10 }}
+            svg={{ fontSize: 10, fill: "black" }}
+          />
+        </View>
+      </View>
+
+      <BarraExpandible>
+        <DetalleRendimiento />
+      </BarraExpandible>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   contenedor: {
