fec3f24ed46aae32af94a94e92363146e69f40e5
Jbarcelo-polarier <jbarcelo.polarier@outlook.com>
2025-04-28 15:04:49 +0200

    actualizado la carga de los datos

--- src/pages/components/DetalleRendimientoDesplegable.js
+++ src/pages/components/DetalleRendimientoDesplegable.js
@@ -5,14 +5,32 @@ import { PERSONA_ID } from "../Index";
 import RendimientoUtils from "../../helpers/RendimientoUtils";
 
 const DetalleRendimientoDesplegable = ({ datos, cargando }) => {
-  const [horasTrabajadas, setHorasTrabajadas] = useState(0);
+  const [horasTrabajadas, setHorasTrabajadas] = useState("00:00:00");
   const [totalPrendas, setTotalPrendas] = useState(0);
   const [mediaPrendasHora, setMediaPrendasHora] = useState(0);
+  const [horaCargada, setHoraCargada] = useState(false);
 
   useEffect(() => {
     console.log("detalles datos", datos);
     cargarDatos(PERSONA_ID, datos.inicioIso, datos.finIso);
-  });
+  }, [horaCargada]);
+
+  const mostrarHoras = () => {
+    const horas = horasTrabajadas.split(":")[0];
+    const minutos = horasTrabajadas.split(":")[1];
+    const segundos = horasTrabajadas.split(":")[2];
+    const horasTexto = `${horas}h ${minutos}m`;
+    console.log(
+      "texto horas Treabajadas",
+      horas,
+      "h",
+      minutos,
+      "m",
+      segundos,
+      "s"
+    );
+    return horasTexto;
+  };
   const cargarDatos = async (idPersona, fechaIni, fechaFin) => {
     const fechaInicio = new Date(fechaIni).toISOString().split("T")[0];
     const fechaFinal = new Date(fechaFin).toISOString().split("T")[0];
@@ -24,16 +42,20 @@ const DetalleRendimientoDesplegable = ({ datos, cargando }) => {
     );
 
     console.log("DRD getDetallesPersona", datos);
-    setHorasTrabajadas(datos.TiempoTrabajado || 0);
-    setTotalPrendas(datos.TotalPrendas || 0);
-    setMediaPrendasHora(datos.MediaPrendasHora || 0);
+    setHorasTrabajadas(datos.TiempoTrabajado);
+    setTotalPrendas(datos.TotalPrendas);
+    setMediaPrendasHora(datos.MediaPrendasHora);
+    setHoraCargada(true);
   };
 
+  if (!horaCargada) {
+    return null;
+  }
   return (
     <View style={[styles.contenidoVisualizacion, cargando && styles.cargando]}>
       <View style={styles.seccionMetrica}>
         <Text style={styles.tituloMetrica}>Horas Trabajadas</Text>
-        <Text style={styles.valorMetrica}>{horasTrabajadas} hrs</Text>
+        <Text style={styles.valorMetrica}>{mostrarHoras()}</Text>
       </View>
 
       <View style={styles.divisor} />
