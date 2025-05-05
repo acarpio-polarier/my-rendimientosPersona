ec5c8a683bea77a4c93b9e69c4649995e003bf46
Jbarcelo-polarier <jbarcelo.polarier@outlook.com>
2025-05-05 08:27:57 +0200

    dependencias borradas

--- src/constants/connection.js
+++ src/constants/connection.js
@@ -1,7 +1,7 @@
+const baseURL = "http://jbarcelo.polarier.com/";
+webApiCoreURL = baseURL;
+
 export const connectionConstants = {
-  WEB_API_URL: "https://localhost:7136/odata/",
-  ODATA_URL: "https://localhost:7136/odata/",
-  REPORT_URL: "https://testreportservice.polarier.com:446/",
-  WEB_API_CORE_ODATA_URL: "https://localhost:7136/odata/",
-  WEB_API_CORE_URL: "https://localhost:7136/",
+  WEB_API_CORE_URL: webApiCoreURL,
+  ODATA_URL: webApiCoreURL + "odata/",
 };
