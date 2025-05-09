1c941b83dc782adf2fac4986c958633a19fc6d88
Jbarcelo-polarier <jbarcelo.polarier@outlook.com>
2025-05-09 13:07:14 +0200

    logica para salir de la pantalla

--- src/pages/Videos/Components/PaginaVideo.js
+++ src/pages/Videos/Components/PaginaVideo.js
@@ -6,13 +6,17 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
-import React, { useState, useEffect } from "react";
+import { AppState } from "react-native";
+
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import YoutubePlayer from "react-native-youtube-iframe";
 import { colors } from "../../../../styles/base";
-import { Switch, SegmentedButtons } from "react-native-paper";
+import { Switch } from "react-native-paper";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import RendimientoUtils from "../../../helpers/RendimientoUtils";
+import { useNavigation } from "@react-navigation/native";
 
 export const PERSONA_ID = 1392;
 
@@ -24,15 +28,71 @@ const PaginaVideo = ({ route }) => {
   const [visto, setVisto] = useState(false);
   const [descCom, setDescCom] = useState("desc");
   const [nuevoComentario, setNuevoComentario] = useState(true);
+  const [tiempoReproducido, setTiempoReproducido] = useState(0);
+  const [duracionVideo, setDuracionVideo] = useState(0);
+  const [youtubeVisto, setYoutubeVisto] = useState(false);
+  const playerRef = useRef(null);
+  const intervalRef = useRef(null);
+  const appState = useRef(AppState.currentState);
+  const navigation = useNavigation();
+
   const fechaEnvio = new Date(video.fechaEnvio).toISOString().split("T")[0];
 
-  // Calculo para el tamaño dle video
   const widthV = Dimensions.get("window").width * 0.9;
   const heightV = (widthV * 9) / 16;
-  const colorNaranja = "'rgba(237, 182, 55, 0.9)'";
+
+  // Logica para detectar si sales de la app
+  useEffect(() => {
+    const handleAppStateChange = (nextAppState) => {
+      if (appState.current.match(/active/) && nextAppState === "background") {
+        console.log("La app pasó a segundo plano");
+      }
+      appState.current = nextAppState;
+    };
+
+    const subscription = AppState.addEventListener(
+      "change",
+      handleAppStateChange
+    );
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  // Logica para detectar si sales de la pagina
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("beforeRemove", () => {
+      console.log("Antes de salir de la pantalla");
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  //Borrar
+  useEffect(() => {
+    console.log(tiempoReproducido);
+  }, [tiempoReproducido]);
+
+  // Actualizar idEstado
+  useEffect(() => {
+    const setEstado = async (idEstado) => {
+      await RendimientoUtils.setIdEstado(video.idPersonaVideo, idEstado);
+    };
+    if (visto) {
+      setEstado(5); // 5 = visto
+    } else {
+      setEstado(3); // 3 = abierto
+    }
+  }, [visto]);
 
   useEffect(() => {
-    if (video.idEstado == 5) setVisto(true);
+    if (video.idEstado === 5) setVisto(true);
+
+    // Limpiar el intervalo
+    return () => {
+      if (intervalRef.current) clearInterval(intervalRef.current);
+    };
   }, []);
 
   const ToggleDescCom = (value) => {
@@ -44,18 +104,35 @@ const PaginaVideo = ({ route }) => {
     setVisto(!visto);
   };
 
-  // Actualizar idEstado
-  useEffect(() => {
-    console.log("PV video", video);
-    const setEstado = async (idEstado) => {
-      await RendimientoUtils.setIdEstado(video.idPersonaVideo, idEstado);
-    };
-    if (visto) {
-      setEstado(5); // 5 = visto
-    } else {
-      setEstado(3); // 3 = abierto
+  const handleEvent = async (event) => {
+    console.log("Evento:", event);
+
+    // Sumador de segundos
+    if (event === "playing") {
+      if (!intervalRef.current) {
+        intervalRef.current = setInterval(() => {
+          setTiempoReproducido((prev) => prev + 1);
+        }, 1000);
+      }
     }
-  }, [visto]);
+
+    // Parar el sumados
+    if (event === "paused" || event === "buffering" || event === "ended") {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+    }
+
+    if (
+      event === "ended" ||
+      (tiempoReproducido / duracionVideo >= 0.95 && duracionVideo > 0)
+    ) {
+      setYoutubeVisto(true);
+      setVisto(true);
+      console.log(tiempoReproducido);
+    }
+  };
 
   return (
     <View style={{ paddingTop: insets.top + 1 }}>
@@ -64,10 +141,17 @@ const PaginaVideo = ({ route }) => {
       </View>
       <View style={styles.contenedorVideos}>
         <YoutubePlayer
+          ref={playerRef}
           height={heightV}
           width={widthV}
           videoId={idVideo}
           play={false}
+          onChangeState={handleEvent}
+          onReady={async () => {
+            const duracion = await playerRef.current.getDuration();
+            setDuracionVideo(duracion);
+            console.log("duración del video", duracion, "segundos");
+          }}
         />
       </View>
       <ScrollView style={styles.descripcionVideo}>
@@ -169,7 +253,6 @@ const styles = StyleSheet.create({
     width: "100%",
     alignSelf: "center",
     marginVertical: "2%",
-    // textAlign: "justify",
     paddingHorizontal: "2%",
   },
   contenedorVideos: {
